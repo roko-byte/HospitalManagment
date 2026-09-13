c411e3bf31bab1f3dfc6ffbe5ac3667b86a41aa4
Aravind Adunuri <Aravind>
2026-09-13 10:07:55 +0530

    Added CreateDoctorRequestDto file

---
+++ src/main/java/com/hospital/dto/CreateDoctorRequestDto.java
@@ -0,0 +1,38 @@
+package com.hospital.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateDoctorRequestDto {
+	
+	private Long userId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private Long phone;
+	
+	private String specialization;
+	
+	private String qualification;
+	
+	private int experienceYears;
+	
+	private String licenseNumber;
+	
+	private BigDecimal  consultationFee;
+	
+	private LocalDate joiningDate;	
+
+
+}
